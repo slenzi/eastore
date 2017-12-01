ee33d415af053a17ecf64e29ddc310cebfea3011
slenzi <lenzi@jimmy.harvard.edu>
2017-12-01 17:30:27 -0500

    bug fix

--- eastore/src/main/java/org/eamrf/eastore/core/service/tree/file/secure/SecurePathResourceTreeService.java
+++ eastore/src/main/java/org/eamrf/eastore/core/service/tree/file/secure/SecurePathResourceTreeService.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -572,7 +573,7 @@ public class SecurePathResourceTreeService {
 				.collect(Collectors.toList());
 		}
 		
-		return null;
+		return new ArrayList<PathResource>();
 		
 	}
 	
